c9add44de088d683078fadd923bc6b6d1a3f2a5b
nzhirov007-byte <n.zhirov007@gmail.com>
2026-09-22 23:38:06 +0300

    Bump KinoRuTest plugin version to 2

--- KinoRuTest/build.gradle.kts
+++ KinoRuTest/build.gradle.kts
@@ -1,2 +1,2 @@
-version=1
+version=2
 cloudstream { description="Русский тестовый каталог с открытым видео"; authors=listOf("nzhirov007-byte"); status=3; tvTypes=listOf("Movie"); language="ru"; isCrossPlatform=true }
