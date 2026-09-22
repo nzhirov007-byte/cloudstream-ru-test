7fcf4349e7b316cada3e9ad2cd7c46a15e887979
nzhirov007-byte <n.zhirov007@gmail.com>
2026-09-22 22:53:30 +0300

    Add KinoRuTest/build.gradle.kts

---
+++ KinoRuTest/build.gradle.kts
@@ -0,0 +1,2 @@
+version=1
+cloudstream { description="Русский тестовый каталог с открытым видео"; authors=listOf("nzhirov007-byte"); status=3; tvTypes=listOf("Movie"); language="ru"; isCrossPlatform=true }
